d6c97739c47889061bb54fa454761b1efe14e505
Subramanya <subramanyanagabhushan@gmail.com>
2022-03-13 05:36:36 +0000

    minor changes to audio and video

--- video/clip_feature_extraction.py
+++ video/clip_feature_extraction.py
@@ -10,27 +10,11 @@ import math
 
 def get_nearest_frame(time, floor_or_ceil=None):
     """Obtain the nearest frame for a given time, video fps, and feature window."""
-    return floor_or_ceil(int(time * 30 / 16)) 
+    return floor_or_ceil(int(time * 30 / 16))
 
 def load_json(path):
     with open(path, "r") as f:
         return json.load(f)
-
-def getFeatures(args, paths):
-    json_data = load_json(args['annotations_path'])
-    for video_data in tqdm(json_data['videos']):
-        _vid = video_data['video_uid']
-        if (_vid in paths.keys()):
-            #print(_vid)
-            video_rep = torch.load(paths[_vid])
-            for clip_data in video_data['clips']:
-                _cid = clip_data['clip_uid']
-                clip_path = os.path.join(args['clip_feature_save_directory'], _cid+'.pt')
-                _vs_sec = get_nearest_frame(clip_data['video_start_sec'], math.floor)
-                _ve_sec = get_nearest_frame(clip_data['video_end_sec'], math.ceil)
-                _clip_rep = video_rep[_vs_sec: _ve_sec +1, :]
-                torch.save(_clip_rep, clip_path)
-                #print(_cid, _vs_sec, _ve_sec, clip_data['video_start_sec'], clip_data['video_end_sec'], _clip_rep.shape[0], _ve_sec-_vs_sec )
 
 def get_all_audio_features_files(args):
     paths = defaultdict(str)
@@ -40,8 +24,22 @@ def get_all_audio_features_files(args):
                 paths[file.split('.')[0]] = os.path.join(root, file)
 
     print("Number of audio files found in ",args['input_video_feature_directory']," is ", len(paths.keys()))
-    return paths        
+    return paths
 
+def getFeatures(args, paths):
+    json_data = load_json(args['annotations_path'])
+    for video_data in tqdm(json_data['videos']):
+        _vid = video_data['video_uid']
+        if (_vid in paths.keys()):
+            audio_rep = torch.load(paths[_vid])
+            for clip_data in video_data['clips']:
+                _cid = clip_data['clip_uid']
+                clip_path = os.path.join(args['clip_feature_save_directory'], _cid+'.pt')
+                os.remove(clip_path)
+                _vs_sec = get_nearest_frame(clip_data['video_start_sec'], math.floor)
+                _ve_sec = get_nearest_frame(clip_data['video_end_sec'], math.ceil)
+                _clip_audio_rep = audio_rep[_vs_sec:_ve_sec + 1, :]
+                torch.save(_clip_audio_rep, clip_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
